110e655a27bb5ad72e4e94f77910de651c595f55
Andrey Pravosudov <studiodec8@gmail.com>
2025-03-23 14:47:51 +0300

    feat: added basic frontend functionality

---
+++ frontend/src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#550055',
+    },
+  },
+  typography: {
+    button: {
+      textTransform: 'none',
+      fontWeight: 400,
+    },
+  },
+});
