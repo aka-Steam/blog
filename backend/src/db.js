088cdce79d5e0dec55dc5e0fb2cc93e2d12f59b6
Maksim <maxkhirug@gmail.com>
2025-03-25 19:05:46 +0300

    WIP: CI изменен способ поиска .env

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
