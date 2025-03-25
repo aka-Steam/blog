a11ea77489b7a92ad6c1d81065d4a0e6b28f0a12
Maksim <maxkhirug@gmail.com>
2025-03-25 18:54:20 +0300

    WIP: CI ретраи подключения к бд и логирование user/port

--- backend/src/db.js
+++ backend/src/db.js
@@ -11,8 +11,24 @@ const sequelize = new Sequelize('blog', process.env.DB_USER, process.env.DB_PASS
 });
 
 const initialize = async () => {
+
+    console.log('DB_USER:', process.env.DB_USER);
+    console.log('DB_PORT:', process.env.DB_PORT);
+
     try {
-        await sequelize.authenticate();
+        let retries = 5;
+        while (retries) {
+            try {
+                console.log('Trying to connect to the database...');
+                await sequelize.authenticate();
+                console.log('Connection established!');
+                break;
+            } catch (error) {
+                retries -= 1;
+                console.log(`Retrying to connect. Attempts left: ${retries}`);
+                await new Promise(res => setTimeout(res, 5000));  // задержка в 5 секунд
+            }
+        }
         await sequelize.sync();
     } catch (error) {
         console.error(`Error initializing database: ${error}`);
